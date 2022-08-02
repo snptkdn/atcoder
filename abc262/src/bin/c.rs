bb407af37abc88986147f0604555788b991aba22
snptkdn <400m.wflick@gmail.com>
2022-08-03 00:31:59 +0900

    abc262 c:TLE

--- abc262/src/bin/c.rs
+++ abc262/src/bin/c.rs
@@ -1,5 +1,24 @@
-use proconio::{input};
+use proconio::input;
+use std::cmp::*;
 
 fn main() {
-    todo!();
+    input!(N: usize, vec: [usize; N]);
+
+    let mut count = 0;
+
+    //println!("{:?}", vec);
+
+    for i in vec.iter().enumerate() {
+        for j in vec[i.0 + 1..].iter().enumerate() {
+            let min = min(*i.1, *j.1);
+            let max = max(*i.1, *j.1);
+
+            //println!("min:{} max:{} i:{} j:{}", min, max, i.0 + 1, i.0 + j.0 + 2);
+            if min == i.0 + 1 && max == i.0 + j.0 + 2 {
+                count += 1;
+            }
+        }
+    }
+
+    println!("{}", count);
 }
